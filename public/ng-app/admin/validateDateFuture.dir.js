52154f78aedb940e02873b11851540588c2c9f40
Kim Maida <git@kim-maida.com>
2015-04-27 22:45:08 -0400

    FIX typo in date validator.

--- public/ng-app/admin/validateDateFuture.dir.js
+++ public/ng-app/admin/validateDateFuture.dir.js
@@ -12,9 +12,6 @@
 		validateDateFutureLink.$inject = ['$scope', '$elem', '$attrs', 'ngModel'];
 
 		function validateDateFutureLink($scope, $elem, $attrs, ngModel) {
-
-			// TODO: this will not accept today as a valid date. Today should be valid.
-
 			var _now = new Date(),
 				_yesterday = _now.setDate(_now.getDate() - 1);;
 
@@ -29,7 +26,7 @@
 
 			ngModel.$formatters.unshift(function(value) {
 				var _d = Date.parse(value),
-					_valid = _yesterday - _d > 0;
+					_valid = _yesterday - _d < 0;
 
 				ngModel.$setValidity('future', _valid);
 				return value;
