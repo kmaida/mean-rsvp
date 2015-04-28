a4756d23fb8abc4a36a77f54459127fdf3c900ec
Kim Maida <git@kim-maida.com>
2015-04-27 22:43:56 -0400

    FIX: validate date future validation to allow today's date.

--- public/ng-app/admin/validateDateFuture.dir.js
+++ public/ng-app/admin/validateDateFuture.dir.js
@@ -15,11 +15,12 @@
 
 			// TODO: this will not accept today as a valid date. Today should be valid.
 
-			var _now = new Date();
+			var _now = new Date(),
+				_yesterday = _now.setDate(_now.getDate() - 1);;
 
 			ngModel.$parsers.unshift(function(value) {
 				var _d = Date.parse(value),
-					_valid = _now - _d < 0;
+					_valid = _yesterday - _d < 0;
 
 				ngModel.$setValidity('future', _valid);
 
@@ -28,7 +29,7 @@
 
 			ngModel.$formatters.unshift(function(value) {
 				var _d = Date.parse(value),
-					_valid = _now - _d > 0;
+					_valid = _yesterday - _d > 0;
 
 				ngModel.$setValidity('future', _valid);
 				return value;
